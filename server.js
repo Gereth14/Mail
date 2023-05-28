2ff19f6885e7f85d1a91534865b28cf23e2c3328
Gereth14 <66812152+Gereth14@users.noreply.github.com>
2023-05-28 00:33:43 +0000

    hide api key

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyparser = require("body-parser");
 const https = require("https");
@@ -31,7 +32,7 @@ app.post("/", function(req, res){
     const url ="https://us10.api.mailchimp.com/3.0/lists/d73adae083"
     const options = {
         method: "POST",
-        auth: "Gereth:c833a4eee552e77062ef9886907a1474-us10"
+        auth: "Gereth:" + process.env.apikeys
     }
     const request = https.request(url, options, function(response){
         if(response.statusCode == 200){
@@ -52,6 +53,3 @@ app.post("/", function(req, res){
 app.listen(process.env.PORT || PORT, function(){
     console.log(`Server is running on: http://localhost:${PORT}`);
 });
-
-// ff4855c87a51c8592369b38e3c570cca-us10
-// d73adae083
